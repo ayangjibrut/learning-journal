a7f6cb92d2ab0001a487a8f28f4513c71a15c976
ayangjibrut <129623348+ayangjibrut@users.noreply.github.com>
2023-08-20 05:49:18 +0700

    Update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const viewMore = document.getElementsByClassName(hidden);
+const viewMore = document.getElementById(hidden);
 
 viewMore.addEventListener("click", function() {
     display = "block"
