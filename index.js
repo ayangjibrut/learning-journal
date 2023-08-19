14be9f174bb528344b775a87370779ed5cdc2e75
ayangjibrut <129623348+ayangjibrut@users.noreply.github.com>
2023-08-20 04:17:15 +0700

    Update .js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const widerScreen = document.getElementById(wider-screen)
+const viewMore = document.getElementsByClassName(hidden);
+
+viewMore.addEventListener("click", function() {
+    display = "block"
+})
