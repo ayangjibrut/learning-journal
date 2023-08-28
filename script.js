a8412ec8e6cf3dda14627928922741ce18b7e234
ayangjibrut <129623348+ayangjibrut@users.noreply.github.com>
2023-08-29 01:02:13 +0700

    fix view-more js

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
+const viewMoreBtn = document.querySelector('#view-more')
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
 
-// navMenu.addEventListener("click", () => {
-//     hamburger.classList.remove("active")
-//     navMenu.classList.remove("active")
-// })
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+navMenu.addEventListener("click", () => {
     hamburger.classList.remove("active")
     navMenu.classList.remove("active")
-}))
-
-const viewMore = document.getElementById("view-more")
-const hidden = document.getElementById("hidden-container") 
-
-viewMore.addEventListener("click", function() {
-    hidden.style.display = "block"
 })
+
+// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+//     hamburger.classList.remove("active")
+//     navMenu.classList.remove("active")
+// }))
+
+let currentItem = 3
+
+viewMoreBtn.onclick = () => {
+    let blogs = [...document.querySelectorAll('.grid-container .blog')]
+    for (let i = currentItem; i < currentItem + 3; i++) {
+        blogs[i].style.display = 'block'
+    }
+
+    currentItem += 3
+
+    if (currentItem >= blogs.length) {
+        viewMoreBtn.style.display = 'none'
+    }
+}
